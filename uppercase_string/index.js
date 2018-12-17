0d6c0b777f2b1110035aa7136bca2c15db7d18e3
yusuf1998 <yusufhabibi1998@gmail.com>
2018-12-17 16:08:49 +0700

    adding file to folder uppercase_string

---
+++ uppercase_string/index.js
@@ -0,0 +1,8 @@
+var first_name = "Yusuf";
+var last_name = "Habibi";
+
+var upperFirstName = first_name.toUpperCase();
+var upperLastName = last_name.toUpperCase();
+
+console.log(upperFirstName);
+console.log(upperLastName);
