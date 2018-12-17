c1d4197e9f1d2074fc5082bdfdecaf4af56be5c2
yusuf1998 <yusufhabibi1998@gmail.com>
2018-12-17 16:58:55 +0700

    edit index.js on folder if_else

--- if_else/index.js
+++ if_else/index.js
@@ -1,6 +1,6 @@
-var n = prompt("Masukan nama anda:");
-var c = confirm(`Hai ${n} Apakah saya tampan?`);
-if (c == true) {
+var nama = prompt("Masukan nama anda:");
+var cond = confirm(`Hai ${n} Apakah saya tampan?`);
+if (cond == true) {
     alert('Oke Terimakasih!!');
 } else {
     alert('Kenapaa ?!!');
