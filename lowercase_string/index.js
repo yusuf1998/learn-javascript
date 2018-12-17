197aa9f180fd161aa391c0ef248d2ed207e084e7
yusuf1998 <yusufhabibi1998@gmail.com>
2018-12-17 16:08:32 +0700

    adding file to folder lowercase_string

---
+++ lowercase_string/index.js
@@ -0,0 +1,8 @@
+var first_name = "Yusuf";
+var last_name = "Habibi";
+
+var lowerFirstName = first_name.toLowerCase();
+var lowerLastName = last_name.toLowerCase();
+
+console.log(lowerFirstName);
+console.log(lowerLastName);
