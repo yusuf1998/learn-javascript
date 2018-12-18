320875597e6e20af68d73d867354a5f06209c236
yusuf1998 <yusufhabibi1998@gmail.com>
2018-12-19 05:20:46 +0700

    adding new file to initial-variable-disp;ay folder

---
+++ initial-variable-display/index.js
@@ -0,0 +1,13 @@
+const names = [
+  "Mr. Fahri",
+  "Mr. Abdul",
+  "Mrs. Josephhine",
+  "Mr. Joseph",
+  "Mr. Paul",
+  "Mrs. Paula",
+  "Mr. Hakim"
+];
+
+for (name in names) {
+  console.log(names[name]);
+}
